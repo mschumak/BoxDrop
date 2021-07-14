e6e7485d5b2fd1535f89476e5a50abcc7552be11
mschumak <ma.schumaker@gmail.com>
2021-07-14 18:14:25 -0400

    Save a flat image of the square within the new box

--- BoxDrop.h
+++ BoxDrop.h
@@ -1,6 +1,6 @@
 /*=============================================================================
  *
- *  Copyright (c) 2019 Sunnybrook Research Institute
+ *  Copyright (c) 2021 Sunnybrook Research Institute
  *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
@@ -47,25 +47,58 @@ class BoxDrop: public AlgorithmBase {
 public:
 	BoxDrop();
 	virtual ~BoxDrop();
+
 private:
 	virtual void run();
 	virtual void init(const image::ImageHandle& image);
+
 	std::string generateReport() const;
 	void updateIntermediateResult();
 	bool buildPipeline();
-	algorithm::GraphicItemParameter m_region_toProcess;
-	IntegerParameter m_size;
-	int point,xCenter,yCenter;
-	TextFieldParameter m_text;
-	ImageResult m_intermediate_result;
-	Rectangle m_rect;
-	GraphicStyle m_style;
-	std::string m_name;
+
+private:
+    ///Define the save file dialog options outside of init
+    sedeen::file::FileDialogOptions defineSaveFileDialogOptions();
+
+    ///Save the separated image to a TIF/PNG/BMP/GIF/JPG flat format file
+    bool SaveFlatImageToFile(const std::string &p);
+
+    ///Given a full file path as a string, identify if there is an extension and return it
+    const std::string getExtension(const std::string &p);
+
+    ///Search the m_saveFileExtensionText vector for a given extension, and return the index, or -1 if not found
+    const int findExtensionIndex(const std::string &x) const;
+
+    ///Check if the file exists and accessible for reading or writing, or that the directory to write to exists
+    static bool checkFile(const std::string &, const std::string &);
+
+private:
+    algorithm::GraphicItemParameter m_region_toProcess;
+    IntegerParameter m_size;
+    int point, xCenter, yCenter;
+    TextFieldParameter m_text;
+    ImageResult m_intermediate_result;
+    Rectangle m_rect;
+    GraphicStyle m_style;
+
+    ///User choice whether to save the image within the box as output
+    BoolParameter m_saveOutputImage;
+    ///Choose what format to write the separated images in
+    OptionParameter m_saveFileFormat;
+    ///User choice of file name stem and type
+    SaveFileDialogParameter m_saveFileAs;
+
+    ///Create a cached factory for faster image saving
+    std::shared_ptr<image::tile::Factory> m_cached_output_factory;
+
+private:
+    std::string m_name;
 	/// The output result
 	algorithm::TextResult m_output_text;
 	OverlayResult m_results;
 	std::string m_type;
 
+    std::vector<std::string> m_saveFileExtensionText;
 };
 } //namespace algorithm
 } //namespace sedeen
